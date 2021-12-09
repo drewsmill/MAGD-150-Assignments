5ecde0a19e2cf4229eee5eecbe515e2c380c6aa2
drewsmill <95499774+drewsmill@users.noreply.github.com>
2021-12-09 16:23:34 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,42 @@
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(300, 300);
   
 }
 
 function draw() {
-  background(80);
-  
+  background(51);
+  let  d = color('black')
+  noFill();
+  stroke(0, 0, 0);
+  bezier(220, 218, 127, 157, 270, 167, 200, 76)
+  bezier(240, 218, 147, 157, 290, 167, 220, 76)
+  stroke(0,0,0);
+  fill(d);
+  ellipse(250, 250, 55, 55);
+  let c = color(235,195,109)
+  fill(c)
   noStroke();
-rect(140, 90, 50, 50);
+  arc(300, 0, 120, 120, HALF_PI, PI);
+  let a = color('#ffffff')
+  fill(a)
+ 
+  triangle(50, 75, 68, 30, 86, 75);
+  triangle(50, 75, 5, 75, 45, 100);
+  triangle(86, 75, 131, 75, 91, 100);
+  triangle(45, 100, 35, 150, 65, 110);
+  triangle(91, 100, 101, 150, 65, 110);
+  beginShape();
+vertex(50, 75);
+vertex(86, 75);
+vertex(91, 100);
+vertex(65, 110);
+vertex(45, 100);
+  endShape(CLOSE);
+  let  b = color(240,100,100,40)
+  fill(b)
+  quad(93, 37, 120, 20, 104, 63, 85, 70);
+  quad(18, 10, 30, 3, 25, 40, 20, 43);
+  quad(110, 160, 120, 150, 200, 200, 90, 180);
+  quad(28, 231, 86, 220, 69, 276, 30, 263);
   
-  rect(10, 90, 50, 50);
-  strokeWeight(4);
-stroke(200);
-rect(135, 138, 60, 60);
-rect(5, 138, 60, 60);
-  strokeWeight(2);
-stroke(40);
-  line(20, 88, 20, 45);
-line(145, 88, 155, 15);
-line(185, 88, 175, 15);
-  strokeWeight(5.0);
-strokeCap(ROUND);
-line(35, 86, 35, 30);
-
-ellipse(165, 60,30, 30);
-ellipse(165, 20,20, 20);
-  strokeWeight(6); 
-point(20, 45);
-  strokeWeight(13); 
-point(35, 30);
 }
-
